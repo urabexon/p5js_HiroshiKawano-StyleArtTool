95af659999c956971cf733c3ead654dbecc63d84
urabexon <hiroki708.a48uk@gmail.com>
2025-05-25 21:02:34 +0900

    init sketch

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var marginH, marginV;
+var gsize;
+var tLen;
+var cLen;
+var rowLen, rowLenCurrent;
+var w;
+var rWPre;
+var rW;
+var colNum;
+var cW, cH, realW, realH;
+var gap;
+var colorMin, colorMax;
+var wMin, wMax;
+var seed;
+
+var colors = [
+    
+];
+
+
+function setup() {
+
+}
